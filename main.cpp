4d15781f73322daccec484bf2f4d10ee98abfd5a
Сергей Синицын <sinicyn@rza.ru>
2024-03-01 18:43:02 +0300

    Clean repo

--- main.cpp
+++
@@ -1,9 +0,0 @@
-#include <iostream>
-#include "square.hpp"
-
-int main()
-{
-    std::cout << "Square of 5 is " << square(5) << std::endl;
-
-    return 0;
-}
--- square.cpp
+++
@@ -1,6 +0,0 @@
-#include "square.hpp"
-
-int square(int x)
-{
-    return x * x + 5;
-}
